fe6d7792f0cc4b32a3039c473d37b501779b6e81
X00136708 Ronan Doran <X00136708@computing.stu.it-tallaght.ie>
2019-10-07 11:42:38 +0100

    First push

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+const host = process.env.JS_HOST || 'localhost';
+const port = process.env.JS_PORT || 3000;
+const app = express();
+
+app.use(helmet());
+
+app.get("/", function (request, response){
+    response.send("Hello World");
+});
+
+const server = app.listen(port, host, () => console.log(`Server listens on https://${host}:${port}`))
