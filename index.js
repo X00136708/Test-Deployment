290134e5c415f230cb564d64ff06448763a12c7d
X00136708 Ronan Doran <X00136708@COMPUTING.STU.IT-TALLAGHT.IE>
2019-10-07 12:26:58 +0100

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(helmet());
 
 app.get("/", function (request, response){
-    response.send("Hello Worldssss");
+    response.send("Hello Worldssssssss");
 });
 
 const server = app.listen(port, host, () => console.log(`Server listens on https://${host}:${port}`))
